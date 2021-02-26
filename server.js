9c450fcfffb9a14f415619d2ef0ab0b7c4e7fd18
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-25 21:04:07 -0500

    updated get requests to include party names

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const db = new sqlite3.Database('./db/election.db', err => {
 
 //get Single Candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name AS party_name
+              FROM candidates LEFT JOIN parties
+              ON candidates.party_id = parties.id
+              WHERE candidates.id = ?`;
   const params = [req.params.id];
   db.get(sql, params, (err, row) => {
     if(err) {
@@ -74,7 +77,9 @@ app.post('/api/candidate', ({ body }, res ) => {
 
 //Get all candidates
 app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name AS party_name
+              FROM candidates LEFT JOIN parties
+              ON candidates.party_id = parties.id`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
